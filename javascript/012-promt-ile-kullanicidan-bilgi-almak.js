00765a448a81e809d0b2883f12fcacbc23a56cbd
Hasan YALÇIN <yalcin.05@outlook.com>
2023-07-06 14:28:16 +0300

    promt ile kullanicidan bilgi almak eklendi

---
+++ javascript/012-promt-ile-kullanicidan-bilgi-almak.js
@@ -0,0 +1,24 @@
+//! DERS-12
+
+// *** PROMT İLE KULLANİCİDAN BİLGİ ALMAK *** //
+
+// promt icerigini console yazdirma islemi
+// let fullName = prompt("Lütfen adınızı giriniz?")
+// console.log(fullName); 
+
+// oge secimi yaparak yazdirma islemi
+// let greeting = document.querySelector("#greeting");
+// greeting.innerHTML = `${greeting.innerHTML} <small style = "color:aqua"> ${fullName} </small>`
+
+
+//! Ekstra Ornekler
+// function istek() {
+//     var kelime=prompt("Bir değer giriniz","lütfen sadece kelime giriniz..")
+//     document.write(kelime)
+// }  
+// "Bir deger giriniz" stringi kullanıcıya gösterilendir. "Lütfen sadece bir deger giriniz" ise Text(input) icerisinde gosterilecek degerdir.
+// var a = prompt("Bir deger giriniz ","lüften sadece bir deger giriniz");
+
+// alert() ile promt degerini dondurmek.
+let deger = prompt("Bir deger giriniz");
+alert(deger);
