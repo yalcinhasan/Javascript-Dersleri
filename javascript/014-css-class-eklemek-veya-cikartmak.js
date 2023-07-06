1c7a6b392fffe1e85f6bd3590be6201f18f236ca
Hasan YALÇIN <yalcin.05@outlook.com>
2023-07-06 15:46:28 +0300

    ekstra ornekler eklendi

--- javascript/014-css-class-eklemek-veya-cikartmak.js
+++ javascript/014-css-class-eklemek-veya-cikartmak.js
@@ -14,3 +14,18 @@ greeting.classList.remove("title","second-class","third-class"); // coklu class 
 
 
 console.log(greeting.classList);
+
+//! Ekstra Onrekler
+
+// item() : HTML de class'ı verilen index sırasına göre döndürür. Eğer index, class length'inden (sayısından) 
+// daha büyük veya length'ine eşit olursa undefined döner.
+console.log(greeting.classList.item(0));
+console.log(greeting.classList.item(3)); // null dondu. :)
+
+// contains() : Element verilen class'ı içeriyorsa true, içermiyorsa false döner. Bu sayede bir işlem yaptırmadan önce kontrol edebiliriz
+console.log(greeting.classList.contains('title')); // false donecektir.
+console.log(greeting.classList.contains('new-info')); // true donecektir.
+
+// replace() : Bir CSS sınıfını başka bir sınıfla değiştirmek için kullanılır.
+
+// length() : Bir öğede bulunan sınıf sayısını bilmemizi sağlar.
