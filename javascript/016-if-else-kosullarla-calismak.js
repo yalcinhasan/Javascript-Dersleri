3cb69f26c9961287472d5bc9c4dcccf9816dcaa6
Hasan YALÇIN <yalcin.05@outlook.com>
2023-07-08 14:44:30 +0300

    ekstra

--- javascript/016-if-else-kosullarla-calismak.js
+++ javascript/016-if-else-kosullarla-calismak.js
@@ -6,11 +6,25 @@
 // eger (username.length > 0) {console.log(username)} degilse {console.log("bilgi yok")} // konusma dili bu sekilde olan yapının kodu;
 // if (username.length > 0) {console.log(username)} else {console.log("bilgi yok")}
 
-let username = prompt("Kullanici adinizi giriniz: ")
+// let username = prompt("Kullanici adinizi giriniz: ")
 
-if (username.length > 0) {  // if kısmı her zaman true ise calisir.
-    console.log(`Kullanici bilginiz: ${username}`)
-}
+// if (username.length > 0) {  // if kısmı her zaman true ise calisir.
+//     console.log(`Kullanici bilginiz: ${username}`)
+// }
+// else {
+//     console.log("Bilgi yok!")
+// }
+
+
+//! Ekstra Ornekler 
+// Tahmin Oyunu: Bilgisayar tarafından 10 'a kadar tutulmuş olan rastgele sayıyı bulalım:
+
+var randomSayi = Math.floor(Math.random() * 10);
+// Girdigimiz sayıyı aklında tutması icin promt degiskene atanır.
+var tahmin = prompt("Bir sayı gir ve rastgele sayıyı bulmaya çalış!");
+if (tahmin === randomSayi && tahmin != null) alert("Bildin !!!");
+else if (tahmin == "") alert("Tahmin yapmadınız!");
+else if (tahmin == null) alert("Tahmin yapmaktan vazgeçtiniz");
 else {
-    console.log("Bilgi yok!")
+    alert("Bir daha dene :( , Random sayi: " + randomSayi);
 }
